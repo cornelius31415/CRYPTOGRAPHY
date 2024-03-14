5f76132038a932a434d56cdf7049091771cc7658
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-03-14 08:14:42 +0100

    Add files via upload

---
+++ Quantum Secure Encryption/LDOTS.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 08:09:34 2024
+
+@author: cornelius
+"""
+
+
+# ----------------------------------- LDOTS ------------------------------------------
+"""
+                        Lamport Diffie One Time Signature 
+                    
+                1. Turn a message string into a sequence of k bits
+                2. Create a key with k columns and 2*k rows consisting
+                   of random 0s and 1s (k is the security parameter)
+                3. Create a Verification Key by hashing every element
+                   of the key
+                4. Create a signature for the message with the key. Every bit
+                   of the message gets signed seperately. For every bit of the
+                   message the column ( 2 * column_index + bit ) becomes part
+                   of the signature.
+                5. Verify the signature of the message with the help of the
+                   Verification Key. The corresponding columns have to match
+                   the hashes of the columns of the signature.
+            
+
+"""
+
+
+
+# ------------------------------------ IMPORTS --------------------------------
+
+import numpy as np
+import hashlib
+
+
+# --------------------------------- MESSAGE -----------------------------------
+
+message = "hello"
+# Turn message into sequence of bits
+message =  ''.join(format(ord(i), '08b') for i in message)    
+message = [int(i) for i in message]  
+
+print(message)
+print()
+
+
+# --------------------------------- KEY GENERATION ---------------------------
+
+# Security Parameter has to be the length of the message
+
+
+k = len(message)                                        # Security Parameter
+key = np.random.randint(0,2,size=(2*k,k)).tolist()      # Key to create Signature
+
+
+def verification_key(key):
+    
+    verification_key = []
+    
+    for c in key:
+        column = []
+        for r in c:
+            column.append(hashlib.sha256(str(r).encode()).hexdigest())
+        
+        verification_key.append(column)
+        
+    return verification_key
+
+
+verify_key = verification_key(key)
+
+
+# ------------------------------- SIGNATURE ----------------------------------
+
+
+def signature(message,key):
+    
+    signature = []
+    for idx, bit in enumerate(message):
+        signature.append(key[2*idx+bit])
+    
+    return signature
+    
+
+signature = signature(message,key)    
+
+
+
+# ----------------------------- VERIFICATION ---------------------------------
+
+"""
+
+1. The Recipient receives (Message, Verfication Key, Signature, hash-function)
+2. Recipient hashes each element of the Signature
+3. Hashed Elements of the Signature get compared with the corresponding
+    hashes in the Verification Key using the Message Bits as Reference
+
+"""
+
+
+def verification(message,verification_key,signature):
+    
+    hashed_signature = []
+    
+    for c in signature:
+        column = []
+        for r in c:
+            column.append(hashlib.sha256(str(r).encode()).hexdigest())
+        
+        hashed_signature.append(column)
+    
+    
+    for idx, bit in enumerate(message):
+        
+        if verification_key[2*idx+bit] != hashed_signature[idx]:
+            return False
+        
+        else:
+            return True
+    
+ 
+
+verify = verification(message, verify_key, signature)
+
+# ---------------------------- Testing the Code -------------------------------
+
+print(signature)
+print()
+print(verify_key)
+print()
+print(verify)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
