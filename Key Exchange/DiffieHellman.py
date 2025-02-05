681f39091e3a690cb05214948dc596d3cbd2f2b7
cornelius31415 <cornelius@22a39b36-02ef-4fc0-b1b2-298fdd01607d.fritz.box>
2025-02-05 18:12:03 +0100

    brought Diffie-Hellman almost to an end but it is more for demonstration not for use...

--- Key Exchange/DiffieHellman.py
+++ Key Exchange/DiffieHellman.py
@@ -14,16 +14,87 @@ Alice und Bob einigen sich auf:
 
 Primzahl und Primitivwurzel können öffentlich bekannt sein
 
+Part-Key Alice
 Alice wählt eine natürliche Zahl a in {1,2,...,p-2} zufällig
 Alice berechnet A = g**a mod p
 Alice schickt A an Bob und hält a geheim
 
+Part-Key Bob
 Bob wählt eine natürliche Zahl b in {1,2,...,p-2} zufällig
 Bob berechnet B = g**b mod p
-Bob schickt 
+Bob schickt B an Alice und hält b geheim
 
-
+Der Schlüssel ist K = g**(ab) mod p
+Für Bob heißt das K = A**b mod p
+Für Alice dann    K = B**a mod p
 
 
 
 """
+
+from PrimitiveRoot import primitiveRoot, generatePrime
+from random import randint
+
+
+# Step 1: Generate prime number p
+# Step 2: Determine primitve root of p
+# Step 3: Calculate the Part-Key
+# Step 4: Calculate the Key
+
+
+p = generatePrime(int(1e4))
+g = primitiveRoot(p)
+
+def partKey(p,g):
+    
+    a = randint(0, p-1)
+    A = pow(g, a, p)
+    return (A,a)
+    
+
+
+
+
+def Key(part_key,exponent):
+    
+    key = pow(part_key[0], exponent,p)
+    return key
+    
+p = 17
+g = primitiveRoot(p)
+
+print(partKey(p,g))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Key Exchange/PrimitiveRoot.py
+++ Key Exchange/PrimitiveRoot.py
@@ -22,12 +22,103 @@ Created on Wed Feb  5 17:19:37 2025
             
             Prime Numbers always have a primitive root
             
+            gcd(g,n) = 1
+            
 """
+# -----------------------------------------------------------------------
+#                                 IMPORTS   
+# -----------------------------------------------------------------------
+
+from math import sqrt,gcd
+from random import choice
+
+# -----------------------------------------------------------------------
+#                              MATH FUNCTIONS   
+# -----------------------------------------------------------------------
+
+
+# NUMBERS RELATIVELY PRIME TO n
+def relativelyPrime(n):
+    
+    relativePrime = []
+    
+    for i in range(1,n):
+        if gcd(i,n) == 1:
+            relativePrime.append(i)
+    
+    return relativePrime
+    
+# CHECK IF NUMBER IS PRIME
+def is_prime(n):
+    
+    for i in range(2,int(sqrt(n)+1)):
+        if n % i == 0:
+            return False
+        
+    return True
+
+# GENERATE LIST OF PRIMES SMALLER THAN n
+def primes(n):
+    
+    primes = [i for i in range(2,n+1) if is_prime(i)]
+
+    return primes
+
+
+
+def generatePrime(Range):
+    
+    primeList = primes(Range)
+    prime = choice(primeList)
+    return prime
+
+
+print(generatePrime(1000))
+
+
+# -----------------------------------------------------------------------
+#                           PRIMITIVE ROOTS   
+# -----------------------------------------------------------------------
 
 
 # determines the primitive root of a number n
-def bruteForce(n):
+def primitiveRoot(n):
     
+    # original set
+    relativePrime = relativelyPrime(n)
     
+    for g in relativePrime:
+        testList = [pow(g,i,n) for i in range(0,len(relativePrime))]
+  
+        if set(testList) ==  set(relativePrime):
+            return g
+        
+    return False
     
-    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
